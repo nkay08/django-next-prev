dd9a845f4892069322a2aa74fb3c53f78baf53dd
nkay08 <niklas.klipphahn@gmail.com>
2019-04-12 14:49:51 +0200

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -15,102 +15,145 @@ class NextPrevTestCase(TestCase):
 
         self.post1 = Post.objects.create(
             title='Post 1', text='Beautiful is better than ugly.',
-            category=self.first_category, created=date(2015, 1, 1))
+            category=self.first_category, created=date(2015, 1, 1),
+            author='Steve')
         self.post2 = Post.objects.create(
             title='Post 2', text='Simple is better than complex.',
             category=self.second_category, created=date(2015, 1, 1))
         self.post3 = Post.objects.create(
             title='Post 3',
             text='Complex is better than complicated.',
-            category=self.first_category, created=date(2016, 1, 1))
+            category=self.first_category, created=date(2016, 1, 1),
+            author='Mary')
 
-    def test_default(self):
-        qs = Post.objects.all()
+    # def test_default(self):
+    #     qs = Post.objects.all()
+    #     first = qs.first()
+    #     self.assertEqual(first, self.post1)
+    #
+    #     second = next_in_order(first)
+    #     self.assertEqual(second, self.post2)
+    #
+    #     third = next_in_order(second)
+    #     self.assertEqual(third, self.post3)
+    #
+    #     fourth = next_in_order(third)
+    #     self.assertEqual(fourth, None)
+    #
+    #     fourth_loop = next_in_order(third, loop=True)
+    #     self.assertEqual(fourth_loop, self.post1)
+    #
+    #     prev = prev_in_order(first)
+    #     self.assertEqual(prev, None)
+    #
+    #     prev_loop = prev_in_order(first, loop=True)
+    #     self.assertEqual(prev_loop, self.post3)
+    #
+    # def test_custom(self):
+    #     qs = Post.objects.all().order_by('category__title', '-created')
+    #
+    #     print(qs)
+    #
+    #     first = qs.first()
+    #     self.assertEqual(first, self.post3)
+    #
+    #     second = next_in_order(first, qs)
+    #     self.assertEqual(second, self.post1)
+    #
+    #     third = next_in_order(second, qs)
+    #     self.assertEqual(third, self.post2)
+    #
+    #     fourth = next_in_order(third, qs)
+    #     self.assertEqual(fourth, None)
+    #
+    #     fourth_loop = next_in_order(third, qs, loop=True)
+    #     self.assertEqual(fourth_loop, self.post3)
+    #
+    #     prev = prev_in_order(first, qs)
+    #     self.assertEqual(prev, None)
+    #
+    #     prev_loop = prev_in_order(first, qs, loop=True)
+    #     self.assertEqual(prev_loop, self.post2)
+    #
+    #     # print(first)
+    #     #
+    #     # second = next_in_order(first, qs)
+    #     # print(second)
+    #     #
+    #     # prev = prev_in_order(first, qs)
+    #     # print(prev)
+    #
+    # def test_no_ordering(self):
+    #     qs = Post.objects.all().order_by()
+    #
+    #     first = qs.first()
+    #     self.assertEqual(first, self.post1)
+    #
+    #     second = next_in_order(first, qs)
+    #     self.assertEqual(second, self.post2)
+    #
+    #     third = next_in_order(second, qs)
+    #     self.assertEqual(third, self.post3)
+    #
+    #     fourth = next_in_order(third, qs)
+    #     self.assertEqual(fourth, None)
+    #
+    #     fourth_loop = next_in_order(third, qs, loop=True)
+    #     self.assertEqual(fourth_loop, self.post1)
+    #
+    #     prev = prev_in_order(first, qs)
+    #     self.assertEqual(prev, None)
+    #
+    #     prev_loop = prev_in_order(first, qs, loop=True)
+    #     self.assertEqual(prev_loop, self.post3)
+    #
+    # def test_empty_ordering(self):
+    #     qs = Category.objects.all()
+    #
+    #     first = qs.first()
+    #     self.assertEqual(first, self.first_category)
+    #
+    #     second = next_in_order(first, qs)
+    #     self.assertEqual(second, self.second_category)
+    #
+    #     third = next_in_order(second, qs)
+    #     self.assertEqual(third, None)
+    #
+    #     third_loop = next_in_order(second, qs, loop=True)
+    #     self.assertEqual(third_loop, self.first_category)
+    #
+    #     prev = prev_in_order(first, qs)
+    #     self.assertEqual(prev, None)
+    #
+    #     prev_loop = prev_in_order(first, qs, loop=True)
+    #     self.assertEqual(prev_loop, self.second_category)
+        
+    def test_order_on_nullable(self):
+        qs = Post.objects.all().order_by('author')
+        print("QS", qs)
         first = qs.first()
-        self.assertEqual(first, self.post1)
+        self.assertEqual(first, self.post2)
 
-        second = next_in_order(first)
-        self.assertEqual(second, self.post2)
-
-        third = next_in_order(second)
-        self.assertEqual(third, self.post3)
-
-        fourth = next_in_order(third)
-        self.assertEqual(fourth, None)
-
-        fourth_loop = next_in_order(third, loop=True)
-        self.assertEqual(fourth_loop, self.post1)
-
-        prev = prev_in_order(first)
-        self.assertEqual(prev, None)
-
-        prev_loop = prev_in_order(first, loop=True)
-        self.assertEqual(prev_loop, self.post3)
-
-    def test_custom(self):
-        qs = Post.objects.all().order_by('category__title', '-created')
-        first = qs.first()
-        self.assertEqual(first, self.post3)
-
+        print("1")
         second = next_in_order(first, qs)
-        self.assertEqual(second, self.post1)
+        self.assertEqual(second, self.post3)
 
+        print("2")
         third = next_in_order(second, qs)
-        self.assertEqual(third, self.post2)
+        self.assertEqual(third, self.post1)
 
+        print("3")
         fourth = next_in_order(third, qs)
         self.assertEqual(fourth, None)
 
+        print("4")
         fourth_loop = next_in_order(third, qs, loop=True)
-        self.assertEqual(fourth_loop, self.post3)
+        self.assertEqual(fourth_loop, self.post2)
 
+        print("5")
         prev = prev_in_order(first, qs)
         self.assertEqual(prev, None)
 
+        print("6")
         prev_loop = prev_in_order(first, qs, loop=True)
-        self.assertEqual(prev_loop, self.post2)
-
-    def test_no_ordering(self):
-        qs = Post.objects.all().order_by()
-
-        first = qs.first()
-        self.assertEqual(first, self.post1)
-
-        second = next_in_order(first, qs)
-        self.assertEqual(second, self.post2)
-
-        third = next_in_order(second, qs)
-        self.assertEqual(third, self.post3)
-
-        fourth = next_in_order(third, qs)
-        self.assertEqual(fourth, None)
-
-        fourth_loop = next_in_order(third, qs, loop=True)
-        self.assertEqual(fourth_loop, self.post1)
-
-        prev = prev_in_order(first, qs)
-        self.assertEqual(prev, None)
-
-        prev_loop = prev_in_order(first, qs, loop=True)
-        self.assertEqual(prev_loop, self.post3)
-
-    def test_empty_ordering(self):
-        qs = Category.objects.all()
-
-        first = qs.first()
-        self.assertEqual(first, self.first_category)
-
-        second = next_in_order(first, qs)
-        self.assertEqual(second, self.second_category)
-
-        third = next_in_order(second, qs)
-        self.assertEqual(third, None)
-
-        third_loop = next_in_order(second, qs, loop=True)
-        self.assertEqual(third_loop, self.first_category)
-
-        prev = prev_in_order(first, qs)
-        self.assertEqual(prev, None)
-
-        prev_loop = prev_in_order(first, qs, loop=True)
-        self.assertEqual(prev_loop, self.second_category)
+        self.assertEqual(prev_loop, self.post1)
